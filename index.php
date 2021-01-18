bcef5f8f11d301372b888257732812504458c911
Elogeek <elodiechristin@gmail.com>
2021-01-18 13:52:53 +0100

    exo 2 sur PHP : les variables

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Exo PHP : les variables</title>
+</head>
+<body>
+    <?php
+
+        $text = "Du texte est stocké ici";
+        echo $text. "<br>";
+        $y = 30;
+        echo "y vaut $y". "<br>";
+
+        $m = 5;
+        $n = 7;
+        echo $m + $n. "<br>";
+
+    ?>
+</body>
+</html>
